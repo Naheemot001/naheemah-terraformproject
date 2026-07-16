8dfdb09c063fb78676a807f46c023a08433f2ec2
FirstName LastName <naheemotpop@yahoo.com>
2026-07-15 21:05:27 -0400

    updated variable file

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "naheemah_app" {
   key_name        = "Ajay_devs"
   security_groups = [aws_security_group.naheemah_app_sg.name]
   tags = {
-    Name = "naheemah-app ${var.environment}"
+    Name = "naheemah-app-${var.environment}"
   }
   user_data = file("userdata.sh")
 }
